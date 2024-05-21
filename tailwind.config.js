915658eaa3e620d3e037af9f3142e5e2233da675
adnan4498 <akhan123456008@gmail.com>
2024-05-21 17:12:59 +0500

    changed url of background image of england in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'england-banner-image': "url('../../public/images/inside-visa-images/england/england-1.jpg')", 
-        'england-banner-image-2': "url('../../public/images/inside-visa-images/england/england-2.jpg')", 
+        'england-banner-image': "url('../public/images/inside-visa-images/england/england-1.jpg')", 
+        'england-banner-image-2': "url('../public/images/inside-visa-images/england/england-2.jpg')", 
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
